b703d02fc8eae3943c1389b8ca6376da01aab9fa
minwook.lee <minwook-lee@naver.com>
2021-07-12 23:06:47 +0900

    기본 UI구성

---
+++ umate/CollectionView/HomeCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  HomeCollectionViewCell.swift
+//  umate
+//
+//  Created by 이민욱 on 2021/07/12.
+//
+
+import UIKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
