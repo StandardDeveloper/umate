8c6224dfce44fdc5414d219ce747a9dfcb475bf3
minwook.lee <minwook-lee@naver.com>
2021-07-12 20:34:10 +0900

    프로젝트 생성 및 gitignore 파일 셋팅

---
+++ umate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  umate
+//
+//  Created by 이민욱 on 2021/07/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
